0f1eaa6922df9ada7fbdd4169a5c078272f6d755
Izzy <avrisaac555@gmail.com>
2020-05-12 01:18:35 -0700

    Memorize function to useful

--- src/useful.fs
+++ src/useful.fs
@@ -8,3 +8,10 @@ module internal Interop  =
     let inline curry f x1 x2 = f(x1, x2)
 
     let inline curry2 f x1 x2 x3 = f(x1, x2, x3)
+
+open System.Collections.Concurrent
+
+module internal Memorize =
+    let inline memorize (f: 'key -> 'value) =
+        let cache = ConcurrentDictionary<'key, 'value> ()
+        fun key -> cache.GetOrAdd(key, f)
